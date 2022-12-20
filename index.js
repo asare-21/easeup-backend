06ae08c4fd2ab1de1c4e6c436b3a13bab3a327c9
Joseph Asare <napoleon@Josephs-MacBook-Pro.local>
2022-12-20 23:16:50 +0000

    updates

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var admin = require("firebase-admin");
 const vhost = require('vhost');
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 1000 requests per `window` (here, per 15 minutes)
+    max: 1000, // Limit each IP to 1000 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: { msg: 'Too many requests from this IP, please try again later', status: 429, success: false },
