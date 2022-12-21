35a04802ef9c4b859d902816c1b1a2b435e678a1
Joseph Asare <napoleon@Josephs-MacBook-Pro.local>
2022-12-21 23:12:43 +0000

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,16 @@ router.get('/', (req, res) => {
     return res.render('index',);
 }
 );
+router.get('/nda', (req, res) => {
+    console.log(req.headers)
+    return res.render('nda',);
+}
+);
+router.get('/terms', (req, res) => {
+    console.log(req.headers)
+    return res.render('terms',);
+}
+);
 
 
 module.exports.HOME = router;
