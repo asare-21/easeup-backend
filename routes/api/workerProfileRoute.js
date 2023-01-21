9a4dc56008b02a0cd6206e50b742b924edd83bac
Joseph Asare <napoleon@Josephs-MacBook-Pro.local>
2023-01-21 20:22:29 +0000

    updates

--- routes/api/workerProfileRoute.js
+++ routes/api/workerProfileRoute.js
@@ -6,24 +6,14 @@ const { workerProfileModel } = require('../../models/worker_profile_model');
 
 const { commonError, returnUnAuthUserError } = require('./user_route')
 
-router.get('/', async (req, res) => {
-    const required_fields = ['uid', "worker_profile_id"]
-    const missing_fields = required_fields.filter(field => !req.body[field])
-    if (missing_fields.length > 0) {
-        // return error of a field is misising
-        return res.status(400).json({
-            msg: 'Missing fields',
-            status: 400,
-            success: false,
-            missing_fields
-        })
-    }
-    const { uid, worker_profile_id } = req.body
+router.get('/:worker', async (req, res) => {
+
+    const { worker } = req.params
 
     // check if user is authenticated
     try {
-        await admin.auth().getUser(uid) // check if uid is valid
-        workerProfileModel.findById(worker_profile_id, (err, worker) => {
+        await admin.auth().getUser(worker) // check if uid is valid
+        workerProfileModel.findById(worker, (err, worker) => {
             if (err) {
                 return commonError(res, err.message)
             }
