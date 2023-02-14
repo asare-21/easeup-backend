ebe82476d07fc2a21693620c8f1958569b4c8276
Joseph Asare <napoleon@Josephs-MacBook-Pro.local>
2023-02-14 16:39:06 +0000

    testing booking

--- routes/api/workerProfileRoute.js
+++ routes/api/workerProfileRoute.js
@@ -522,6 +522,7 @@ router.get('/booking-completed/:worker', async (req, res) => {
 
 router.post('/book-slot', async (req, res) => {
     const { worker, client, skills, start, end, name } = req.body
+    console.log(req.body)
     try {
         if (!start || !end) return commonError(res, 'Please provide all required fields. Start and End times are required.')
         //code to check if start and end date are valid
