b29b5fb01d875d053e53c1b1f5d57772f280a2c3
Joseph Asare <napoleon@Josephs-MacBook-Pro.local>
2023-03-02 22:46:22 +0000

    corrections

--- routes/api/workerProfileRoute.js
+++ routes/api/workerProfileRoute.js
@@ -657,10 +657,10 @@ router.post('/book-slot', async (req, res) => {
         if (!date || !end) return commonError(res, 'Please provide all required fields. Start and End times are required.')
         //code to check if start and end date are valid
         if (!isValidDate(new Date(date))) {
-            return commonError(res, 'Please provide valid dates. ' + (i === 0 ? 'Start' : 'End') + ' date is invalid.')
+            return commonError(res, 'Please provide valid dates.  date is invalid.')
         }
         if (!isValidDate(new Date(end))) {
-            return commonError(res, 'Please provide valid dates. ' + (i === 0 ? 'Start' : 'End') + ' date is invalid.')
+            return commonError(res, 'Please provide valid dates.  date is invalid.')
         }
         if (!worker || !client || !skills || !name || !fee || !ref || !image || !workerImage) return commonError(res, 'Please provide all required fields. Worker, Client, Skills, Fee...')
         await admin.auth().getUser(worker) // check if worker is valid
