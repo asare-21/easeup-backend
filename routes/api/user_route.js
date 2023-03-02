e38520a01fadbe40893fef252c3105c029f1a380
Joseph Asare <napoleon@Josephs-MacBook-Pro.local>
2023-03-02 23:04:44 +0000

    updates

--- routes/api/user_route.js
+++ routes/api/user_route.js
@@ -409,7 +409,7 @@ router.post('/create', async (req, res) => {
         const existingWorker = await workerModel.findById(user_id).exec()
         if (existingWorker) {
             // Worker Already Exists
-            return res.status(200).json({ user: existingWorker, msg: 'Worker exists. Account not created', status: 200, success: true })
+            return res.status(200).json({ user: existingWorker, msg: 'Worker exists. Account not created', status: 200, success: false })
         }
         // check if user already exists
         const userExists = await userModel
