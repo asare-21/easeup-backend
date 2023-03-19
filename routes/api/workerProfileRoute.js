22c585571b3c44c53ba53845bbc7c36ab3e1869b
Joseph Asare <napoleon@Josephs-MacBook-Pro.local>
2023-03-19 16:49:14 +0000

    correction

--- routes/api/workerProfileRoute.js
+++ routes/api/workerProfileRoute.js
@@ -762,8 +762,9 @@ router.post('/book-slot', async (req, res) => {
         // return error if date is in the past
         const workerToken = await workerModel.findById(worker)
         const userToken = await userModel.findById(client)
+        console.log(userToken, workerToken)
         const foundBookings = await bookingModel.find({ worker: worker, isPaid: true, completed: false, cancelled: false, day })
-        console.log(foundBookings)
+        // console.log(foundBookings)
 
         if (foundBookings.length === 3) {
             return commonError(res, 'All slots for the day have been booked. Please select another day.')
@@ -801,14 +802,14 @@ router.post('/book-slot', async (req, res) => {
             })
 
             await newBooking.save() // save booking
-            await admin.messaging().send({
+            if (workerToken) await admin.messaging().send({
                 notification: {
                     title: 'New Booking',
                     body: 'You have a new booking. Please check your dashboard for more details.'
                 },
                 token: workerToken.token
             })
-            await admin.messaging().send({
+            if (userToken) await admin.messaging().send({
                 notification: {
                     title: 'New Booking',
                     body: 'Your booking was successful. Awaiting payment.'
