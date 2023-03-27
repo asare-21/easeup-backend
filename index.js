3ff914c1df9997840206bbd342cef05491ee2a9b
Joseph Asare <napoleon@Josephs-MacBook-Pro.local>
2023-03-27 19:31:33 +0100

    updates

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ const { userModel } = require('./models/user_model');
 const { getAndCacheUsers, getAndCacheWorkerMedia, getAndCacheWorkerProfiles, getAndCacheWorkers } = require('./utils');
 const { dashboard } = require('./routes/api/dashboard');
 const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 30 minutes
-    max: 5, // Limit each IP to 1000 requests per `window` (here, per 15 minutes)
+    windowMs: 30 * 60 * 1000, // 30 minutes
+    max: 100, // Limit each IP to 1000 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 
