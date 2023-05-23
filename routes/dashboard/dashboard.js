13a5f27f80274a79d417d7d4bc4c3a34045e2ed0
Joseph Asare <napoleon@Josephs-MacBook-Pro-2.local>
2023-05-23 13:29:29 +0000

    updates

--- routes/dashboard/dashboard.js
+++ routes/dashboard/dashboard.js
@@ -60,6 +60,54 @@ router.get('/users/:uid', async (req, res) => {
 
     }
 })
+router.get('/workers/:uid', async (req, res) => {
+    try {
+        const { uid } = req.params.uid
+        // verify user
+        await admin.auth().getUser(uid)
+
+        const workers = await workerModel.find().count()
+        return res.status(200).json({
+            msg: 'User Profiles',
+            status: 200,
+            success: true,
+            // profiles: workers,
+            count: workers.length
+        })
+    } catch (e) {
+        console.log(e)
+        return res.status(400).json({
+            msg: 'Error fetching worker profiles',
+            status: 400,
+            success: false,
+        })
+
+    }
+})
+router.get('/users/:uid', async (req, res) => {
+    try {
+        const { uid } = req.params.uid
+        // verify user
+        await admin.auth().getUser(uid)
+
+        const users = await userModel.find()
+        return res.status(200).json({
+            msg: 'User Profiles',
+            status: 200,
+            success: true,
+            profiles: users,
+            count: users.length
+        })
+    } catch (e) {
+        console.log(e)
+        return res.status(400).json({
+            msg: 'Error fetching worker profiles',
+            status: 400,
+            success: false,
+        })
+
+    }
+})
 
 
 
