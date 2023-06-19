72f391199df0117eef55a75721e1751371eb3491
Joseph Asare <napoleon@Josephs-MacBook-Pro-2.local>
2023-06-19 19:32:01 +0100

    minor changes

--- routes/api/subscribe.js
+++ routes/api/subscribe.js
@@ -37,24 +37,24 @@ router.post('/mail', async (req, res) => {
         const contact = await
             contactModel.create({ email, phone, company, message, name })
         // send sms/email notification
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'easeup.gh@gmail.com',
-                pass: 'nyZqan-ryvry7-cujqoq'
-            }
-        });
-        const msg = `Name: ${name} \n Email: ${email} \n Phone: ${phone} \n Company: ${company} \n Message: ${message}`
-        var mailOptions = {
-            from: 'easeup.gh@gmail.com',
-            to: 'joseph.asare@easeupgh.tech',
-            subject: 'New Contact',
-            text: msg
-        };
+        // var transporter = nodemailer.createTransport({
+        //     service: 'gmail',
+        //     auth: {
+        //         user: 'easeup.gh@gmail.com',
+        //         pass: 'nyZqan-ryvry7-cujqoq'
+        //     }
+        // });
+        // const msg = `Name: ${name} \n Email: ${email} \n Phone: ${phone} \n Company: ${company} \n Message: ${message}`
+        // var mailOptions = {
+        //     from: 'easeup.gh@gmail.com',
+        //     to: 'joseph.asare@easeupgh.tech',
+        //     subject: 'New Contact',
+        //     text: msg
+        // };
         const response = axios.get(`https://api.smsonlinegh.com/v4/message/sms/send?key=${process.env.EASEUP_SMS_API_KEY}&text=${message}&type=0&sender=${process.env.EASEUP_SMS_SENDER}&to=${phone}`)// wait for the sms to be sent
 
-        const sendmail = transporter.sendMail(mailOptions);
-        await Promise.all([response, sendmail])
+        // const sendmail = transporter.sendMail(mailOptions);
+        await Promise.all([response])
         //////////////////////////////
 
         // issue a redirect to the home page
