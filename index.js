ae4a1c08fb277d1cfb06743317d9ea9d447c0cc5
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2023-07-09 22:01:43 +0000

    updates

--- index.js
+++ index.js
@@ -1,58 +1,72 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(http);
 const PORT = process.env.PORT || 3000;
-const morgan = require('morgan')
-const { connect } = require('mongoose')
-const rateLimit = require('express-rate-limit')
-const { USER_ROUTE } = require('./routes/api/user_route')
-const { searchRoute } = require('./routes/api/searchRoute')
-const { workerProfileVerificationRoute } = require('./routes/api/workerProfileVerify')
-const { workerRoute } = require('./routes/api/workerRoute')
-const { workerProfileRoute } = require('./routes/api/workerProfileRoute')
-const { bookmarkRoute } = require('./routes/api/bookmarkRoute');
-const log = require('npmlog')
+const morgan = require("morgan");
+const { connect } = require("mongoose");
+const rateLimit = require("express-rate-limit");
+const { USER_ROUTE } = require("./routes/api/user_route");
+const { searchRoute } = require("./routes/api/searchRoute");
+const {
+  workerProfileVerificationRoute,
+} = require("./routes/api/workerProfileVerify");
+const { workerRoute } = require("./routes/api/workerRoute");
+const { workerProfileRoute } = require("./routes/api/workerProfileRoute");
+const { bookmarkRoute } = require("./routes/api/bookmarkRoute");
+const log = require("npmlog");
 const serviceAccount = require("./easeup.json");
 const FBadmin = require("firebase-admin");
-const compression = require('compression')
-const helmet = require('helmet');
-const { chatRoute } = require('./routes/api/chat');
-const { chatRoomModel } = require('./models/chatRoomModel');
-const { chatModel } = require('./models/chat_message_model');
-const { workerModel } = require('./models/worker_models');
-const { userModel } = require('./models/user_model');
-const { dashboard } = require('./routes/dashboard/dashboard');
-const { jobPlanRoute } = require('./routes/api/job_plan_route');
+const compression = require("compression");
+const helmet = require("helmet");
+const { chatRoute } = require("./routes/api/chat");
+const { chatRoomModel } = require("./models/chatRoomModel");
+const { chatModel } = require("./models/chat_message_model");
+const { workerModel } = require("./models/worker_models");
+const { userModel } = require("./models/user_model");
+const { dashboard } = require("./routes/dashboard/dashboard");
+const { jobPlanRoute } = require("./routes/api/job_plan_route");
 const authRoutes = require("./routes/api/auth");
-const { jobs } = require('./routes/api/jobs');
+const { jobs } = require("./routes/api/jobs");
 const limiter = rateLimit({
-    windowMs: 100, // 
-    max: 1000,
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: true, // Disable the `X-RateLimit-*` headers
-    message: { msg: 'Too many requests from this IP, please try again later', status: 429, success: false, limit: true },
-    onLimitReached: (req, res, options) => {
-        log.warn(`Rate limit reached for IP: ${req.ip}`)
-        return res.status(429).json({ msg: 'Too many requests from this IP, please try again later', status: 429, success: false, limit: true })
-    }
-})
-
+  windowMs: 100, //
+  max: 1000,
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: true, // Disable the `X-RateLimit-*` headers
+  message: {
+    msg: "Too many requests from this IP, please try again later",
+    status: 429,
+    success: false,
+    limit: true,
+  },
+  onLimitReached: (req, res, options) => {
+    log.warn(`Rate limit reached for IP: ${req.ip}`);
+    return res
+      .status(429)
+      .json({
+        msg: "Too many requests from this IP, please try again later",
+        status: 429,
+        success: false,
+        limit: true,
+      });
+  },
+});
 
 const session = require("express-session");
 const { verifyJWT } = require("./passport/common");
 const passport = require("passport");
 require("./passport/passport");
-const cors = require('cors');
-const { subscribeRoute } = require('./routes/api/subscribe');
-const { recommendationRoute } = require('./routes/api/recommendation');
+const cors = require("cors");
+const { subscribeRoute } = require("./routes/api/subscribe");
+const { recommendationRoute } = require("./routes/api/recommendation");
 
 //Auth
 app.use(
   session({
     secret: "cat",
+    cookie: { secure: false },
   })
 );
 app.use(passport.initialize());
@@ -68,197 +82,200 @@ app.get("/authenticated-route", verifyJWT, (req, res) => {
 });
 
 // Static files
-app.enable('trust proxy');
-app.use(cors())
-app.use(compression())
-app.use(helmet())
-app.disable('x-powered-by');
+app.enable("trust proxy");
+app.use(cors());
+app.use(compression());
+app.use(helmet());
+app.disable("x-powered-by");
 app.use(express.json());
-app.use(morgan('combined'))
-app.use(limiter)
+app.use(morgan("combined"));
+app.use(limiter);
 app.use("/auth", authRoutes);
 
-app.use('/user', USER_ROUTE);
-app.use('/search', searchRoute)
-app.use('/bookmark', bookmarkRoute)
-app.use('/verify-worker-profile', workerProfileVerificationRoute)
-app.use('/worker', workerRoute)
-app.use('/worker-profile', workerProfileRoute)
-app.use('/room', chatRoute)
-app.use('/jobs', jobs)
-app.use('/subscribe', subscribeRoute)
-app.use('/recommend', recommendationRoute)
-app.use('/jplan', jobPlanRoute)
-app.use('/dashboard', dashboard)
+app.use("/user", USER_ROUTE);
+app.use("/search", searchRoute);
+app.use("/bookmark", bookmarkRoute);
+app.use("/verify-worker-profile", workerProfileVerificationRoute);
+app.use("/worker", workerRoute);
+app.use("/worker-profile", workerProfileRoute);
+app.use("/room", chatRoute);
+app.use("/jobs", jobs);
+app.use("/subscribe", subscribeRoute);
+app.use("/recommend", recommendationRoute);
+app.use("/jplan", jobPlanRoute);
+app.use("/dashboard", dashboard);
 // handle 404
 app.use((req, res, next) => {
-    return res.status(404).json({
-        msg: 'Undefined route', status: 404, success: false,
-        path: req.path
-    })
-})
+  return res.status(404).json({
+    msg: "Undefined route",
+    status: 404,
+    success: false,
+    path: req.path,
+  });
+});
 
 // enforce https
 app.use(function (req, res, next) {
-    if (process.env.NODE_ENV != 'development' && !req.secure) {
-        return res.redirect('https://' + req.headers.host + req.url);
-    }
-    next();
-})
-
-
-
-
+  if (process.env.NODE_ENV != "development" && !req.secure) {
+    return res.redirect("https://" + req.headers.host + req.url);
+  }
+  next();
+});
 
 // Starting the server
 http.listen(PORT, async () => {
-    try {
-        log.info(`Listening on port ${PORT}`);
-        await connect(`mongodb+srv://${process.env.easeup_admin_founder_email}:${process.env.easeup_admin_founder_pass}@easeup-cluster.pfxvast.mongodb.net/?retryWrites=true&w=majority`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName: 'easeup',
-
-        })
-        FBadmin.initializeApp({
-            credential: FBadmin.credential.cert(serviceAccount)
-        });
-        log.info("Connected to MongoDB and running")
-
-    } catch (err) {
-        console.error(err)
-    }
-})
-
+  try {
+    log.info(`Listening on port ${PORT}`);
+    await connect(
+      `mongodb+srv://${process.env.easeup_admin_founder_email}:${process.env.easeup_admin_founder_pass}@easeup-cluster.pfxvast.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: "easeup",
+      }
+    );
+    FBadmin.initializeApp({
+      credential: FBadmin.credential.cert(serviceAccount),
+    });
+    log.info("Connected to MongoDB and running");
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 // /////////////////////// Socket.io
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnected", (msg) => {
+    console.log("message: ", msg);
+  });
 
-    console.log('a user connected');
-    socket.on('disconnected', (msg) => {
-        console.log('message: ', msg);
+  socket.on("connected", (msg) => {
+    console.log("message: ", msg);
+  });
+
+  // create chat room
+  socket.on("new-room", async (room) => {
+    /**
+     * new room structure
+     * {
+     * room,
+     * worker,
+     * user
+     * }
+     */
+    await createNewRoom(room);
+    console.log("room created");
+  });
+
+  // join room
+  socket.on("join-room", async (chat) => {
+    socket.join(chat.room); // add socket to room
+    const clients = io.sockets.adapter.rooms[chat.room];
+    console.log("rooms ", io.sockets.adapter.rooms);
+    if (!clients) {
+      console.log(`No clients in room: ${chat.room}`);
+      return;
+    }
+    const clientIds = Object.keys(clients.sockets);
+    console.log(`Clients in room '${chat.room}':`, clientIds);
+    console.log(`Room data sent'${chat.room}':`, chat);
+  });
+  socket.on("message", async (chat) => {
+    // io.to(chat.room).emit('message', chat); // broadcast message to all users except sender
+    socket.broadcast.to(chat.room).emit("message", chat);
+    const worker = await workerModel.findOne({ _id: chat.worker });
+    const user = await workerModel.findOne({ _id: chat.user });
+    // send notification to user or worker
+    await admin.messaging().send({
+      notification: {
+        title: "New Message",
+        body: chat.message,
+      },
+      data: {
+        room: chat.room,
+        user: chat.user,
+        worker: chat.worker,
+        from: chat.from,
+        message: chat.message,
+        media: chat.media,
+      },
+      token: chat.from === chat.user ? worker.token : user.token,
     });
 
-    socket.on('connected', (msg) => {
-        console.log('message: ', msg);
-    });
-
-    // create chat room
-    socket.on('new-room', async (room) => {
-        /**
-         * new room structure
-         * {
-         * room,
-         * worker,
-         * user
-         * }
-         */
-        await createNewRoom(room)
-        console.log('room created')
-    })
-
-    // join room
-    socket.on('join-room', async (chat) => {
-        socket.join(chat.room); // add socket to room
-        const clients = io.sockets.adapter.rooms[chat.room];
-        console.log('rooms ', io.sockets.adapter.rooms)
-        if (!clients) {
-            console.log(`No clients in room: ${chat.room}`);
-            return;
-        }
-        const clientIds = Object.keys(clients.sockets);
-        console.log(`Clients in room '${chat.room}':`, clientIds);
-        console.log(`Room data sent'${chat.room}':`, chat);
-    })
-    socket.on('message', async (chat) => {
-        // io.to(chat.room).emit('message', chat); // broadcast message to all users except sender
-        socket.broadcast.to(chat.room).emit('message', chat);
-        const worker = await workerModel.findOne({ _id: chat.worker })
-        const user = await workerModel.findOne({ _id: chat.user })
-        // send notification to user or worker
-        await admin.messaging().send({
-            notification: {
-                title: 'New Message',
-                body: chat.message
-            },
-            data: {
-                room: chat.room,
-                user: chat.user,
-                worker: chat.worker,
-                from: chat.from,
-                message: chat.message,
-                media: chat.media
-            },
-            token: chat.from === chat.user ? worker.token : user.token
-        })
-
-        await saveChat(chat)
-
-    })
-}
-)
+    await saveChat(chat);
+  });
+});
 // Save chat to database
 async function saveChat(chat) {
-    const { room, user, message, from, worker, media } = chat
-    const newChat = new chatModel({
-        room,
-        user,
-        message,
-        from,
-        worker,
-        media
-    })
-    try {
-        // emit message to user
-        console.log('message sent from', from)
-        console.log('message sent to', from === user ? worker : user)
-        // io.to(room).emit(from === user ? user : worker, chat)
-        // emit mesaage to user before saving to database
-        await newChat.save()
-
-    } catch (err) {
-        console.log(err)
-        io.emit(room, ' Error saving message')
-    }
+  const { room, user, message, from, worker, media } = chat;
+  const newChat = new chatModel({
+    room,
+    user,
+    message,
+    from,
+    worker,
+    media,
+  });
+  try {
+    // emit message to user
+    console.log("message sent from", from);
+    console.log("message sent to", from === user ? worker : user);
+    // io.to(room).emit(from === user ? user : worker, chat)
+    // emit mesaage to user before saving to database
+    await newChat.save();
+  } catch (err) {
+    console.log(err);
+    io.emit(room, " Error saving message");
+  }
 }
-
 
 // Create a new room
 async function createNewRoom(_room) {
-    const { room, worker, user, userName, userPhoto, workerName, workerPhoto } = _room
-    const newRoom = new chatRoomModel({
+  const { room, worker, user, userName, userPhoto, workerName, workerPhoto } =
+    _room;
+  const newRoom = new chatRoomModel({
+    room,
+    worker,
+    user,
+    userName,
+    userPhoto,
+    workerName,
+    workerPhoto,
+  });
+  try {
+    chatRoomModel.findOne(
+      {
         room,
         worker,
         user,
-        userName,
-        userPhoto,
-        workerName,
-        workerPhoto
-    })
-    try {
-        chatRoomModel.findOne({
-            room, worker, user
-        }, async (err, doc) => {
-            if (err) {
+      },
+      async (err, doc) => {
+        if (err) {
+          console.log(err);
+        }
+        if (doc) {
+          console.log("room already exists");
+          io.emit(user, "Room already exists");
+        } else {
+          await newRoom.save();
+          await userModel.findOneAndUpdate(
+            { _id: user },
+            { $push: { rooms: room } }
+          );
+          await workerModel.findOneAndUpdate(
+            { _id: worker },
+            { $push: { rooms: room } }
+          );
 
-                console.log(err)
-            }
-            if (doc) {
-                console.log('room already exists')
-                io.emit(user, 'Room already exists')
-            }
-            else {
-                await newRoom.save()
-                await userModel.findOneAndUpdate({ _id: user }, { $push: { rooms: room } })
-                await workerModel.findOneAndUpdate({ _id: worker }, { $push: { rooms: room } })
-
-                io.emit(user, 'Room created')
-                console.log('room created')
-            }
-        })
-    } catch (e) {
-        console.log('Something went room ', e)
-    }
+          io.emit(user, "Room created");
+          console.log("room created");
+        }
+      }
+    );
+  } catch (e) {
+    console.log("Something went room ", e);
+  }
 }
 
-module.exports.admin = FBadmin
+module.exports.admin = FBadmin;
