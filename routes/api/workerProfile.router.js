8275a99b8ceee8d1e142713e0b2b37b89fb23440
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2023-07-20 06:46:02 +0000

    updates to worker verify

--- routes/api/workerProfile.router.js
+++ routes/api/workerProfile.router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const workerProfileController = require("../../controllers/workerProfile.controller");
-const { verifyJWT } = require("../../passport/common");
+const { workerVerifyJWT } = require("../../passport/common");
 const {
   getReviewsCache,
   getCommentsCache,
@@ -20,144 +20,144 @@ const { getWorkerProfileCache } = require("../../cache/worker_profile");
 
 router.get(
   "/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   getWorkerProfileCache,
   workerProfileController.getWorkerProfile
 );
 
 router.get(
   "/reviews/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   getReviewsCache,
   workerProfileController.getWorkerReviews
 );
 router.get(
   "/comments/:worker/:post",
-  verifyJWT,
+  workerVerifyJWT,
   getCommentsCache,
   workerProfileController.getWorkerComments
 );
 router.post(
   "/comments/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.addWorkerComments
 );
-router.post("/charge", verifyJWT, workerProfileController.addWorkerCharge);
-router.post("/skills", verifyJWT, workerProfileController.addWorkerSkills);
-router.post("/bio", verifyJWT, workerProfileController.addWorkerBio);
-router.post("/instagram", verifyJWT, workerProfileController.addWorkerInstagram);
-router.post("/portfolio", verifyJWT, workerProfileController.addWorkerPortfolio);
+router.post("/charge", workerVerifyJWT, workerProfileController.addWorkerCharge);
+router.post("/skills", workerVerifyJWT, workerProfileController.addWorkerSkills);
+router.post("/bio", workerVerifyJWT, workerProfileController.addWorkerBio);
+router.post("/instagram", workerVerifyJWT, workerProfileController.addWorkerInstagram);
+router.post("/portfolio", workerVerifyJWT, workerProfileController.addWorkerPortfolio);
 router.get(
   "/portfolio/:worker/:page",
-  verifyJWT,
+  workerVerifyJWT,
   mediaCache,
   workerProfileController.getWorkerPortfolio
 );
 router.post(
   "/work-radius",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.addWorkerRadius
 );
 router.get(
   "/booking/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   getBookingCache,
   workerProfileController.getBooking
 );
 router.get(
   "/paid/:user",
-  verifyJWT,
+  workerVerifyJWT,
   getPaidBookingsCache,
   workerProfileController.getPaidBooking
 );
 router.get(
   "/booking-upcoming/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   getUpcomingBookingCache,
   workerProfileController.getUpcomingBooking
 );
 router.get(
   "/booking-pending/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.getPendingBooking
 );
 router.get(
   "/booking-progress/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.getBookingInProgress
 );
 router.get(
   "/booking-completed/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   getCompletedBookingCache,
   workerProfileController.getCompletedBooking
 );
 router.get(
   "/booking-cancelled/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   getCancelledBookingCache,
   workerProfileController.getCancelledBooking
 );
 router.put(
   "/booking-status",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.updateBookingStatus
 );
 router.put(
   "/booking-status/pending",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.markBookingAsPending
 );
 router.post(
   "/worker-review",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.addWorkerReviews
 );
 router.get(
   "/worker-review/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   getWorkerReviewCache,
   workerProfileController.getWorkerReviews
 );
 router.post(
   "/available-slots/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.addAvailableSlots
 );
-router.post("/book-slot", verifyJWT, workerProfileController.bookWorkerSlot);
+router.post("/book-slot", workerVerifyJWT, workerProfileController.bookWorkerSlot);
 router.post(
   "/verify-payment",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.verifyPaymentWebhook
 );
 router.post(
   "/refund/:ref",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.refundWorker
 );
 router.post(
   "/cancel/:ref",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.cancelWorker
 );
 router.patch(
   "/update-location",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.updateLocation
 );
 router.patch(
   "/update-date",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.updateDate
 );
 router.get(
   "/notify/:worker",
-  verifyJWT,
+  workerVerifyJWT,
   workerProfileController.notifyWorker
 );
 router.get(
   "/popular/:id",
-  verifyJWT,
+  workerVerifyJWT,
   getPopularWorkersCache,
   workerProfileController.getPopular
 );
